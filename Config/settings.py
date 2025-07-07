d4c7cf9f3fca69fc0b6107e3ab6ba1756f6920e1
Nazimjonovna <sturayeva000@gmail.com>
2025-07-07 23:29:54 +0500

    configurated cors error

--- Config/settings.py
+++ Config/settings.py
@@ -163,6 +163,16 @@ SWAGGER_SETTINGS = {
 }
 
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://kira-l0my.onrender.com",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
 
 CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://kira-l0my.onrender.com",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
